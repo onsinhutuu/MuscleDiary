c709a328467983dbee2c5d181bab0df66bfb4623
vagrant <vagrant@localhost.localdomain>
2020-10-17 05:57:22 +0000

    contact[Modify]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+        port:                 587,
+        address:              'smtp.gmail.com',
+        domain:               'gmail.com',
+        user_name:             ENV["SEND_MAIL"],
+        password:              ENV["SEND_MAIL_PASSWORD"],
+        authentication:       'plain',
+        enable_starttls_auto: true
+  }
 end
