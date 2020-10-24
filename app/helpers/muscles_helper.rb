e973ca0254f58aa02e5da8399dcd9abca7d45a90
vagrant <vagrant@localhost.localdomain>
2020-10-24 09:01:30 +0000

    config/https[Finish]

--- app/helpers/muscles_helper.rb
+++ app/helpers/muscles_helper.rb
@@ -1,6 +1,7 @@
 module MusclesHelper
   def render_with_hashtags(work_tag)
     sanitize work_tag.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/) { |word| link_to word, "/muscle/hashtag/#{word.delete('#')}" }
-    # gsubで/[#＃] = ハッシュタグと　＃についてる文字　＝　\w\p{Han}ぁ-ヶｦ-ﾟー]+/をlink_to wordでリンクにしている。
+    # gsubでハッシュタグ(/[#＃])と＃についてる文字(\w\p{Han}ぁ-ヶｦ-ﾟー]+/)をlink_to wordでリンクにして、sanitizeでhtml化している。
+    # sanitizeを使う理由はXSS対策である。.html_safeだとrubocopにも怒られる
   end
 end
